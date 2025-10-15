4fe521e04d9688be5e91da3d3b1899c187adaf94
amalkoc <armin.malkoc@insparx.com>
2025-10-15 11:58:56 +0200

    changed credentials and repo

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,9 +2,9 @@
 
 def call() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-full-pip', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t 192.168.0.112:3031/demo-app:jma-2.0 .'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-private-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t amalkoc/twn-demo-app:jma-sl-2.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push 192.168.0.112:3031/demo-app:jma-2.0'
+        sh 'docker push amalkoc/twn-demo-app:jma-sl-2.0'
     }
 }
